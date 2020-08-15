5981dff493e319551f3f093acf3cf7d6289667a2
talha <talhaparvez285@gmail.com>
2020-08-15 16:13:06 +0530

    Added Search Filter on backend for products

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,9 @@ const {
   Get_Single_Product,
   Add_New_Product,
   Get_All_Products,
-  get_Related_Products
+  get_Related_Products,
+  listBySearch
+  // list_Categories_with_products
 
 } = require("../controllers/products");
 const auth = require("../middleware/auth");
@@ -14,5 +16,8 @@ router.post("/create",Add_New_Product);
 router.get("/getAll", Get_All_Products);
 router.get("/:id",Get_Single_Product)
 router.get("/related/:id",get_Related_Products)
+router.post("/by/search", listBySearch);
+
+// router.get("/categories",list_Categories_with_products)
 
 module.exports = router;
